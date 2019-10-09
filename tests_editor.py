32ecf4f12f2c5335f5e718c725dc5c2b6f391b14
Martin Moxon <moxon6@googlemail.com>
2019-10-09 12:06:21 +0100

    Simplfiy editor

--- tests_editor.py
+++ tests_editor.py
@@ -2,11 +2,7 @@ from browser import document as doc, window
 from browser import html
 import editor
 
-
 __BRYTHON__.debug = 1
-
-# next functions are defined in editor.py
 doc['show_js'].bind('click',editor.show_js)
-# Create a lambda around editor.run() so that the event object is not passed to it
 doc['run'].bind('click',lambda *args: editor.run())
 doc['show_console'].bind('click',editor.show_console)
