ed951784786f638da9114d67fa15332b6ea1fc68
Martin Moxon <moxon6@googlemail.com>
2019-10-09 12:07:49 +0100

    Remove redundant brython debug

--- tests_editor.py
+++ tests_editor.py
@@ -2,7 +2,6 @@ from browser import document as doc, window
 from browser import html
 import editor
 
-__BRYTHON__.debug = 1
 doc['show_js'].bind('click',editor.show_js)
 doc['run'].bind('click',lambda *args: editor.run())
 doc['show_console'].bind('click',editor.show_console)
